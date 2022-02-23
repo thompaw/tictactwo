8869036f2b02ce14d38e6826cb3d0790333506f5
thompaw <98345171+thompaw@users.noreply.github.com>
2022-02-22 22:51:44 -0500

    changed board structure

--- game.py
+++ game.py
@@ -1,19 +1,18 @@
 class gameBoard:
-    def __init__(self, playermoves, aimoves):
-        self.player_movelist = playermoves
-        self.ai_movelist = aimoves
+    def __init__(self, boardin=[['+', '+', '+'], ['+', '+', '+'], ['+', '+', '+']]):
+        self.board = boardin
 
     def newBoard(self):
-        newb = gameBoard(playermoves=[], aimoves=[])
+        newb = gameBoard(boardin=[['+', '+', '+'], ['+', '+', '+'], ['+', '+', '+']])
         return newb
 
     def displayBoard(self):
         for y in range(3):
             for x in range(3):
                 if x < 2:
-                    print('x', end='|')
+                    print(self.board[y][x], end='|')
                 else:
-                    print('x', end='')
+                    print(self.board[y][x], end='')
             print()
             if y < 2:
                 print('-----')
---
+++ main.py
@@ -0,0 +1,6 @@
+import game
+
+if __name__ == "__main__":
+    g = game.gameBoard()
+    newg = g.newBoard()
+    newg.displayBoard()
