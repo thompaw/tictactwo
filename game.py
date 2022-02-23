7335e83e390701d78f655695cae779796d106222
thompaw <98345171+thompaw@users.noreply.github.com>
2022-02-22 22:43:37 -0500

    updated .gitignore, added to game and cleared main

--- game.py
+++ game.py
@@ -1,12 +1,27 @@
+class gameBoard:
+    def __init__(self, playermoves, aimoves):
+        self.player_movelist = playermoves
+        self.ai_movelist = aimoves
+
+    def newBoard(self):
+        newb = gameBoard(playermoves=[], aimoves=[])
+        return newb
+
+    def displayBoard(self):
+        for y in range(3):
+            for x in range(3):
+                if x < 2:
+                    print('x', end='|')
+                else:
+                    print('x', end='')
+            print()
+            if y < 2:
+                print('-----')
+    
+    def checkWin(self):
+        pass
 
 
-def display_board():
-    for y in range(9):
-        for x in range(9):
-            if ((x + 1) % 3 == 0) and (x < 8):
-                print('x', end='|')
-            else:
-                print('x', end='')
-        print()
-        if ((y + 1) % 3 == 0) and (y < 8):
-            print('-----------')
+class ticTacGame:
+    def __init__(self):
+        pass
