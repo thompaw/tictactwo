ef5485a30ea42fb2ac2aeb46fe08e465da16a3e1
thompaw <98345171+thompaw@users.noreply.github.com>
2022-02-26 16:58:42 -0500

    added all files into one

--- arty.py
+++ arty.py
@@ -1,5 +1,51 @@
-import ai
 import random
+
+
+class Arty:
+    def __init__(self, board):
+        self.b = board
+
+    def scanPotentialWins(self, mark):
+        # rows
+        moveDict = {}
+        for x in range(3):
+            for y in range(3):
+                if self.b[x][y] != mark:
+                    moveDict[(x, y)] += 1
+                else: 
+                    moveDict[(x, y)] = 0
+        # columns
+        for y in range(3):
+            for x in range(3):
+                if self.b[x][y] != mark:
+                    moveDict[(x, y)] += 1
+                else: 
+                    moveDict[(x, y)] = 0
+        # front diagonals
+        for d in range(3):
+            if self.b[d][d] != mark:
+                moveDict[(d, d)] += 1
+            else:
+                moveDict[(d, d)] = 0
+        # rear diagonals
+        for d in range(2, 0, -1):
+            if self.b[d][d] != mark:
+                moveDict[(d, d)] += 1
+            else:
+                moveDict[(d, d)] = 0
+        return moveDict
+
+    def setUp(self):
+        pass
+
+    def nextMove(self):
+        pass
+
+    def artyMove(self):
+        # this is the culmination, where the ai picks a spot
+        xmoveDict = self.scanPotentialWins('X')
+        omoveDict = self.scanPotentialWins('O')
+
 
 class gameBoard:
     def __init__(self, boardin=[['+', '+', '+'], ['+', '+', '+'], ['+', '+', '+']]):
@@ -69,7 +115,7 @@ class ticTacGame:
         self.gboard[move[0]][move[1]] = 'O'
 
     def aiTurn(self):
-        arty = ai.Arty(self.gboard)
+        arty = Arty(self.gboard)
 
     def playGame(self):
         gameWon = False
--- main.py
+++
@@ -1,6 +0,0 @@
-import game
-
-if __name__ == "__main__":
-    g = game.gameBoard()
-    newg = g.newBoard()
-    newg.displayBoard()
